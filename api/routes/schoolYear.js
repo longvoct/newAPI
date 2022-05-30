8b3c0798daeb877a3c2c71d369047ba39a8a368d
longvoct <86196042+longvoct@users.noreply.github.com>
2022-05-30 20:48:32 +0700

    Update schoolYear.js

--- api/routes/schoolYear.js
+++ api/routes/schoolYear.js
@@ -4,11 +4,11 @@ const schoolYearController = require("../controller/schoolYearController");
 // ADD A SCHOOL_YEAR
 router.post("/", schoolYearController.addSchoolYear);
 
-//GET ALL SCHOOL_YEAR
-router.get("/", schoolYearController.getAllSchoolYears);
-
 //GET A SCHOOL_YEAR
 router.get("/:id", schoolYearController.getSchoolYear);
+
+//GET ALL SCHOOL_YEAR
+router.get("/", schoolYearController.getAllSchoolYears);
 
 //UPDATE A SCHOOL_YEAR
 router.put("/:id", schoolYearController.updateSchoolYear);
